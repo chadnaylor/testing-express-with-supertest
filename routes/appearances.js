611bc2ec7bcbc10bf7a2cc5c36ced0a4efccb21b
Chad Naylor <chad.naylor@gmail.com>
2021-01-04 14:48:07 -0600

    Appearances endpoint correctly joins with movies table

--- routes/appearances.js
+++ routes/appearances.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-router.get('/', function(req, res, next) {
-  knex('appearances').then(function (appearances) {
-    res.json(appearances)
-  }).catch(function (err) {
-    next(new Error(err));
-  })
+router.get('/', function (req, res, next) {
+  knex('appearances')
+    .join('movies', 'appearances.movie_id', '=', 'movies.id')
+    .then(function (appearances) {
+      res.json(appearances)
+    }).catch(function (err) {
+      next(new Error(err));
+    })
 });
 
 module.exports = router;
--- test/movies.test.js
+++ test/movies.test.js
@@ -2,12 +2,13 @@ var app = require('../app')
 var request = require('supertest')(app);
 var knex = require('../db/knex')
 
-describe("Movies API", function () {
-    afterAll(async function (done) {
-        const client = knex
-        await client.destroy();
-        await done();
-    });
+afterAll(async function (done) {
+    const client = knex
+    await client.destroy();
+    await done();
+});
+describe("Movies Endpoint", function () {
+
     it("gets the seeded data", function (done) {
         request.get('/api/v1/movies')
             .expect(200)
@@ -20,3 +21,27 @@ describe("Movies API", function () {
     });
 })
 
+describe("Appearances Endpoint", function () {
+    it("gets the seeded data", function (done) {
+        request.get('/api/v1/appearances')
+            .expect(200)
+            .end(function (err, res) {
+                console.log(res.body)
+                if (err) return done(err)
+                expect(res.body.length).toEqual(4)
+                done();
+            });
+    });
+
+    it("returns data with a movie title and release year", function (done) {
+        request.get('/api/v1/appearances')
+            .expect(200)
+            .end(function (err, res) {
+                console.log(res.body)
+                if (err) return done(err)
+                expect(res.body[0].title).toBeDefined()
+                expect(res.body[0].release_year).toBeDefined()
+                done();
+            });
+    });
+})
